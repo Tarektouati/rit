e850e089591a76243b2bca35ad7222c168be7750
TarekTouati <19335073+Tarektouati@users.noreply.github.com>
2021-11-27 19:13:28 +0100

    :sparkles: add optional path option to init command

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,34 @@
 use std::env::args;
 use std::env::current_dir;
 use std::fs;
+use std::path::Path;
+use std::process::exit;
 
+fn create_rit_directories(path: &str) {
+    let git_root_dir = format!("{}/.git", path);
+    let _ = fs::create_dir(&git_root_dir);
+    let folders = ["objects", "refs"];
+    for folder in folders.iter() {
+        let _ = fs::create_dir(format!("{}/{}", &git_root_dir, folder));
+    }
+    exit(0);
+}
 
 fn create_empty_repo () {
     let args: Vec<String> = args().collect();
-    if args.len() == 2 {
-        let path = current_dir().unwrap().display().to_string();
-        println!("Initializing git repository in current directory...");
-        let git_root_dir = format!("{}/.git", path);
-        let _ = fs::create_dir(&git_root_dir);
-        let folders = ["objects", "refs"];
-        for folder in folders.iter() {
-            let _ = fs::create_dir(format!("{}/{}", &git_root_dir, folder));
+    if args.len() == 3 {
+        let path = &args[2];
+        println!("Initializing Rit repository at {}...", path);
+        if !Path::new(path).exists() {
+            eprintln!("error: {} does not exist ", path);
+            exit(1);
         }
-
+        create_rit_directories(path);
     } else {
-        println!("TODO : Initializing git repository at {}...", args[2]);
-    } 
+        let path = current_dir().unwrap().display().to_string();
+        println!("Initializing Rit repository in current directory...");
+        create_rit_directories(&path);
+    }
 }
 
 fn perform_command (command: &str) {
@@ -25,13 +36,15 @@ fn perform_command (command: &str) {
         "help" => {
             println!("Available commands:");
             println!("  help - show this help");
-            println!("  init - run the program");
+            println!("  init [path] - create empty Rit repository");
         },
         "init" => {
             create_empty_repo()
         },
         _ => {
-            println!("Unknown command: {}", command);
+            println!();
+            eprintln!("error: unknown command {}", command);
+            exit(1);
         }
     }
 }
