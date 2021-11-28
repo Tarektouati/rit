094bd8cc7b429290986ef7d0f05e85c72becda00
Tarek Touati <19335073+Tarektouati@users.noreply.github.com>
2021-11-28 21:23:13 +0100

    :sparkles: Add init command (#1)
    * :sparkles: Init git repo in current dir
    
    * :sparkles: add optional path option to init command
    
    * :recycle: cleanup and isolate commands in module
    
    * :green_heart: fix Ci check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env::args;
+use std::process::exit;
+mod cmd;
+
+fn perform_command (command: &str) {
+    match command {
+        "help" => {
+           cmd::help::show_help();
+        },
+        "init" => {
+            cmd::init::create_empty_repo();
+        },
+        _ => {
+            println!();
+            eprintln!("error: unknown command {}", command);
+            exit(1);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = args().collect();
+    let command = &args[1];
+    perform_command(command);
 }
