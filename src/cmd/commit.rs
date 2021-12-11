645494fbda6f521e2277b78ddafcaabdcba8e113
TarekTouati <19335073+Tarektouati@users.noreply.github.com>
2021-12-11 20:10:32 +0100

    :sparkles: store commit tree

--- src/cmd/commit.rs
+++ src/cmd/commit.rs
@@ -1,5 +1,6 @@
 use std::env::current_dir;
 use std::fs;
+use std::path::{PathBuf};
 use walkdir::{DirEntry, WalkDir};
 mod database;
 
@@ -12,28 +13,37 @@ fn read_file(file_path: &String) -> String {
     content
 }
 
+fn prepare_file(file: &PathBuf, path: &String) -> (String, String) {
+    let shortend_path =  format!("{}/", &path);
+    let file_path: String = file.display().to_string();
+    let file_name: String = String::from(&file_path.replace(&shortend_path, ""));
+    let content = read_file(&file_path);
+    (file_name, content)
+}
+
+fn find_files(path: &String) -> Vec<PathBuf> {
+    return WalkDir::new(&path)
+    .into_iter()
+    .filter_map(|f| f.ok())
+    .filter(|f| !is_git_dir(f))
+    .filter(|f| f.file_type().is_file())
+    .map(|f| f.path().to_owned())
+    .collect();
+}
+
 pub fn create_commit() {
     let path = current_dir().unwrap().display().to_string();
-    let files: Vec<_> = WalkDir::new(&path)
-        .into_iter()
-        .filter_map(|f| f.ok())
-        .filter(|f| !is_git_dir(f))
-        .filter(|f| f.file_type().is_file())
-        .map(|f| f.path().to_owned())
-        .collect();
-    
+    let files = find_files(&path);
     let entries: Vec<(String, String)> = files
         .iter()
-        .map(|f| {
-            let file_path: String = f.display().to_string();
-            let content = read_file(&file_path);
-            (file_path, content)
-        })
-        .map(|f| {
-            let (file_path, content) = f;
-            let oid = database::store_file(content);
-            return (oid, file_path);
+        .map(|f| prepare_file(f, &path))
+        .map(|(file_name, content)| {
+            let oid = database::store_file(database::FileType::Blob, content);
+            return (oid, file_name);
         })
         .collect();
-        // create tree from entries
+
+    let tree = database::store_tree(entries);
+    println!("tree: {}", tree);
+    unimplemented!();
 }
--- src/cmd/commit/database.rs
+++ src/cmd/commit/database.rs
@@ -6,6 +6,11 @@ use std::env::current_dir;
 use std::fs;
 use std::io::Write;
 
+pub enum FileType {
+   Blob,
+   Tree,
+}
+
 fn oid_from_str(string: &str) -> String {
    let mut hasher = Sha1::new();
    hasher.input_str(string);
@@ -41,15 +46,38 @@ fn write_object(oid: &String, content: String) {
    }
 }
 
-pub fn store_file(file_content: String) -> String {
+pub fn store_file(object_type: FileType, file_content: String) -> String {
    // encode to ASCII_8BIT
    let byte_string: &[u8] = file_content.as_bytes();
+
+   let object_type: &str = match object_type {
+      FileType::Blob => "blob",
+      FileType::Tree => "tree",
+   };
    // format content to string with  "#{ object.type } #{ string.bytesize }\0#{ string }”
-   let content = format!("blob {}\0{}", byte_string.len(), file_content);
+   let content = format!("{} {}\0{}", object_type , byte_string.len(), file_content);
    // create oid SHA1 of content
    let oid = oid_from_str(content.as_str());
    // write git object
    write_object(&oid, content);
 
    return oid;
+}
+
+pub fn store_tree(entries: Vec<(String, String)>) -> String {
+   // store file tree and return oid
+   const MODE: &str = "100644";
+
+   let mut sorted_entries = entries.clone();
+   sorted_entries.sort();
+   let tree: Vec<_> = sorted_entries
+      .iter()
+      .map(|(oid, name)| {  
+         let s = format!("{} {};{:?}", MODE, name, hex::encode(oid));
+         return s
+      })
+      .collect();
+      println!("tree: {:?}", tree);
+      //unimplemented!();
+      store_file(FileType::Tree, tree.join(""))
 }
