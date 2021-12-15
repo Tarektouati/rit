c1b93f2b737bf4695febe00f33ed1d306fe7794f
TarekTouati <19335073+Tarektouati@users.noreply.github.com>
2021-12-15 19:48:57 +0100

    :bug: store binary instead of plain string

--- src/cmd/commit/tree.rs
+++ src/cmd/commit/tree.rs
@@ -9,13 +9,24 @@ impl Tree {
         Tree { entries: entries }
     }
 
+    fn oid_to_ascii_bytes(&self, oid: String) -> Vec<String> {
+        return oid
+        .as_bytes()
+        .chunks(2)
+         .map(|bytes| bytes[0] + bytes[1])
+         .map(|b| format!("{:x}", b))
+         .map(|s| u8::from_str_radix(&s, 16).map(|n| n as char).unwrap())
+         .map(|c| format!("{}", c))
+         .collect::<Vec<String>>();
+    }
+
     pub fn to_string(&self) -> String {
         let mut sorted_entries = self.entries.clone();
         sorted_entries.sort();
-        let tree: Vec<_> = sorted_entries
+        let tree: Vec<String> = sorted_entries
            .iter()
-           .map(|(oid, name)| format!("{} {};{:?}", MODE, name, hex::encode(oid)))
+           .map(|(oid, name)|  format!("{} {};{}", MODE, name, self.oid_to_ascii_bytes(oid.to_string()).join("")))
            .collect();
-         tree.join("")
+        return tree.join("")
     }
 }
